2cacade1fa8b71a72ea630057aa6671d3fa7189b
phunkleho <kahoc@phm.gov.au>
2017-07-17 01:12:59 +1000

    "comment-whitespace-inside": null added

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,8 +8,9 @@ module.exports = {
     "at-rule-no-unknown": [ true, {
       ignoreAtRules: ["define-mixin", "mixin", "mixin-content"],
     }],
+    "comment-whitespace-inside": null,
     "custom-property-empty-line-before": null,
     "declaration-empty-line-before": null,
-    "selector-list-comma-newline-after": null
+    "selector-list-comma-newline-after": null,
   }
 }
