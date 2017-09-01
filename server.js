d3128fa28cbbb8ab57578c30f5268233904c33ea
phunkleho <kahoc@phm.gov.au>
2017-09-02 02:14:41 +1000

    Testing Apex up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 const pathMatch = require('path-match');
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production' && !process.env.NOW;
 const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,20 +11,20 @@ const route = pathMatch();
 const match = route('/example-page/:id');
 
 app.prepare()
-.then(() => {
-  createServer((req, res) => {
-    const { pathname } = parse(req.url);
-    const params = match(pathname);
+  .then(() => {
+    createServer((req, res) => {
+      const { pathname } = parse(req.url);
+      const params = match(pathname);
 
-    if (params === false) {
-      handle(req, res);
-      return;
-    }
+      if (params === false) {
+        handle(req, res);
+        return;
+      }
 
-    app.render(req, res, '/example-page', params);
-  })
-  .listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  })
-});
+      app.render(req, res, '/example-page', params);
+    })
+    .listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    })
+  });
