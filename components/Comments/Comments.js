44ece66b30904de1ac85991bde8c3cf9b3651d19
unkleho <kahoc@phm.gov.au>
2017-11-29 19:16:25 +1100

    Comments cleanup

--- components/Comments/Comments.js
+++ components/Comments/Comments.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { gql, graphql } from 'react-apollo';
 
 import SectionTitle from '../SectionTitle';
 import Comment from '../Comment';
@@ -13,43 +12,6 @@ class Comments extends Component {
     comments: PropTypes.array,
     postId: PropTypes.number.isRequired,
   };
-
-  // constructor() {
-  //   super();
-  //
-  //   this.state = {
-  //     isFormSubmitted: false,
-  //   };
-  // }
-  //
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  //
-  //   const authorEmail = 'kaho.cheung@sl.nsw.gov.au';
-  //   const authorName = 'Kaho';
-  //   const content = e.target.elements.content.value;
-  //   const postId = this.props.postId;
-  //   const parentId = 0;
-  //
-  //   this.props.submitComment({
-  //     authorEmail,
-  //     authorName,
-  //     content,
-  //     postId,
-  //     parentId,
-  //   })
-  //   .then(() => {
-  //     this.setState({
-  //       isFormSubmitted: true,
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
-  //
-  //   // reset form
-  //   // e.target.elements.content.value = '';
-  // }
 
   render() {
     const {
@@ -92,50 +54,6 @@ class Comments extends Component {
           <SectionTitle>We love feedback</SectionTitle>
 
           <CommentForm postId={postId} />
-          {/* {!this.state.isFormSubmitted ? (
-            <div>
-              <p className="comments__form__intro">Your email address will not be published. Required fields are marked <span>*</span>.</p>
-
-              <form onSubmit={this.handleSubmit}>
-                <div className="comments__form__section">
-                  <label for="name">Name<span>*</span></label>
-                  <input
-                    name="name"
-                    type="text"
-                    aria-required="true"
-                    placeholder="Your name"
-                  />
-                </div>
-
-                <div className="comments__form__section">
-                  <label for="email">Email<span>*</span></label>
-                  <input
-                    name="email"
-                    type="email"
-                    aria-required="true"
-                    placeholder="Your email"
-                  />
-                </div>
-
-                <div className="comments__form__section">
-                  <label for="content">Comment<span>*</span></label>
-                  <textarea
-                    placeholder="Write a comment"
-                    name="content"
-                    aria-required="true"
-                    rows="4"
-                  ></textarea>
-                </div>
-
-                <button className="button" type="submit" aria-label="Comment Submit Button.">Submit</button>
-              </form>
-            </div>
-          ) : (
-            <div>
-              <p>Thanks for your feedback, your comment is awaiting approval.</p>
-            </div>
-          )} */}
-
         </div>
 
         <style jsx>{styles}</style>
@@ -146,34 +64,3 @@ class Comments extends Component {
 }
 
 export default Comments;
-
-// const query = gql`
-//   mutation createComment(
-//     $authorEmail: String,
-//     $authorName: String!,
-//     $content: String!,
-//     $postId: Int!,
-//     $parentId: Int
-//   ) {
-//     createComment(
-//       authorEmail: $authorEmail,
-//       authorName: $authorName,
-//       content: $content,
-//       postId: $postId,
-//       parentId: $parentId
-//     ) {
-//       id
-//       content
-//       authorName
-//       parentId
-//     }
-//   }
-// `;
-//
-// export default graphql(query, {
-//   props: ({ mutate }) => ({
-//     submitComment: (args) => mutate({
-//       variables: args,
-//     }),
-//   }),
-// })(Comments);
