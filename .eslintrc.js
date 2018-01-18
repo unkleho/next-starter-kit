df924eacf773a9f6cb979b653fcb69854356ab93
unkleho <kahoc@phm.gov.au>
2018-01-19 01:16:48 +1100

    Some lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "react-app",
-    "dxlab"
-  ],
-  "globals": {
-    "document": true,
-    "window": true,
-    "process": true,
+  parser: 'babel-eslint',
+  extends: ['react-app', 'dxlab', 'prettier/react'],
+  globals: {
+    document: true,
+    window: true,
+    process: true,
   },
-  "rules": {
-    "comma-dangle": ["warn", "always-multiline"],
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-max-props-per-line": [1, { "maximum": 3 }],
-    "no-tabs": 0,
-  }
-}
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-max-props-per-line': [1, { maximum: 3 }],
+    'no-tabs': 0,
+  },
+};
--- components/Masthead/Masthead.js
+++ components/Masthead/Masthead.js
@@ -6,70 +6,75 @@ import glitchStyles from '../../styles/glitch.css';
 import shuffle from '../../lib/shuffle';
 
 class Masthead extends Component {
-
   static propTypes = {
-    title: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object,
-    ]).isRequired,
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
     subtitle: PropTypes.string,
     text: PropTypes.string,
     sideText: PropTypes.string,
     backgroundImageUrl: PropTypes.string,
     size: PropTypes.string,
-  }
+  };
 
   constructor() {
-    super()
-    this.state={title:' '}
+    super();
+    this.state = { title: ' ' };
   }
 
   componentDidMount() {
-    this.shuffleText(this.props.slug)
+    this.shuffleText(this.props.slug);
   }
 
   // componentDidUpdate(oldProps, state, nextProps) {
   //   console.log(oldProps);
   //   // const t = {
-  //   //  ...this.props.title 
+  //   //  ...this.props.title
   //   // }
 
   //   // console.log(t.title)
 
-  //   // this.shuffleText(t.title)    
+  //   // this.shuffleText(t.title)
   // }
 
-  shuffleText=(t)=>{
+  shuffleText = (t) => {
     const shfl = shuffle(t);
     let offset = 20;
-    shfl.forEach((word)=>{
-
+    shfl.forEach((word) => {
       setTimeout(() => {
         this.setState({
-          title: word
-        })
-      }, offset);    
+          title: word,
+        });
+      }, offset);
       offset += 20;
     });
-  }
+  };
 
   render() {
-    
     return (
-      <div className={`masthead ${this.props.size && `masthead--${this.props.size}`}`}>
+      <div
+        className={`masthead ${this.props.size &&
+          `masthead--${this.props.size}`}`}
+      >
         <div
           className="masthead__bg"
-          style={this.props.backgroundImageUrl && {
-            backgroundImage: `url(${this.props.backgroundImageUrl})`,
-          }}
-        ></div>
+          style={
+            this.props.backgroundImageUrl && {
+              backgroundImage: `url(${this.props.backgroundImageUrl})`,
+            }
+          }
+        />
 
-        <div className={`masthead__content container container--${this.props.size ? this.props.size : 'md'}`}>
+        <div
+          className={`masthead__content container container--${
+            this.props.size ? this.props.size : 'md'
+          }`}
+        >
           {this.props.subtitle && (
             <div className="masthead__subtitle">{this.props.subtitle}</div>
           )}
 
-          <h1 className="masthead__title glitch" data-text={this.props.slug}>{this.state.title}</h1>
+          <h1 className="masthead__title glitch" data-text={this.props.slug}>
+            {this.state.title}
+          </h1>
 
           {this.props.text && (
             <p className="masthead__intro-text">{this.props.text}</p>
@@ -80,12 +85,13 @@ class Masthead extends Component {
 
         {/* {this.props.children} */}
 
+        {/* prettier-ignore */}
         <style global jsx>{styles}</style>
+        {/* prettier-ignore */}
         <style global jsx>{glitchStyles}</style>
       </div>
     );
   }
-
 }
 
 export default Masthead;
