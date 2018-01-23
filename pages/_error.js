6d60f2c7140f2bda4dd051149a6fdde77c2c67be
Luke Dearnley <luke.dearnley@sl.nsw.gov.au>
2018-01-23 12:23:56 +1100

    404 page working now

---
+++ pages/_error.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+
+import App from '../components/App';
+import Masthead from '../components/Masthead';
+
+class Four04 extends Component {
+
+  render() {
+
+    return (
+      <App>
+        <div className="posts container container--lg">
+          <Masthead
+          title="page not found"
+          backgroundImageUrl='/static/images/masthead-search-terms-projection.jpg'
+          slug="page not found"
+        >
+        </Masthead> 
+        </div>
+      </App>
+    );
+  }
+
+}
+
+export default Four04; 
