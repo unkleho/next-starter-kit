e1464b97046fee7074eaf7d30f46021883d9f22a
Kaho Cheung <kaho@elefanttraks.com>
2018-03-06 16:24:54 +1100

    Update search page

--- pages/collection/search.js
+++ pages/collection/search.js
@@ -1,17 +1,19 @@
 import { gql, graphql } from 'react-apollo';
 
 import withData from '../../lib/withData';
-import ExampleApp from '../../components/examples/ExampleApp';
+import App from '../../components/App';
 import Link from '../../components/Link';
-import Header from '../../components/Header';
+// import Header from '../../components/Header';
 import styles from './search.css';
 
-const SearchPage = ({
-  url,
-  items,
-}) => (
-  <ExampleApp>
-    <Header pathname={url.pathname} />
+const SearchPage = ({ url, items, loading: isLoading }) => (
+  <App
+    pathname="/search"
+    isLoading={isLoading}
+    title="Search"
+    metaDescription="{excerpt}"
+  >
+    {/* <Header pathname={url.pathname} /> */}
 
     <div className="search-page">
       <div className="search-form">
@@ -21,36 +23,41 @@ const SearchPage = ({
         </form>
       </div>
 
-      {items && items.map(({ id, sourceRecordId, title, images, type, description }, i) => (
-        <article key={`posts-${i}`}>
-          <Link to={`http://archival.sl.nsw.gov.au/Details/archive/${sourceRecordId}`}>
-            <a>
-              <div className="item__image-holder">
-                {images && images[0].url ? (
-                  <img src={images[0].url} alt={title} />
-                ) : (
-                  <div>No Image</div>
-                )}
-              </div>
+      {items &&
+        items.map(
+          ({ id, sourceRecordId, title, images, type, description }, i) => (
+            <article key={`posts-${i}`}>
+              <Link
+                to={`http://archival.sl.nsw.gov.au/Details/archive/${sourceRecordId}`}
+              >
+                <a>
+                  <div className="item__image-holder">
+                    {images && images[0].url ? (
+                      <img src={images[0].url} alt={title} />
+                    ) : (
+                      <div>No Image</div>
+                    )}
+                  </div>
 
-              <div className="item__info">
-                <div className="item__type">{type}</div>
-                <h2>{title}</h2>
-                <p>{description}</p>
-              </div>
-            </a>
-          </Link>
-        </article>
-      ))}
+                  <div className="item__info">
+                    <div className="item__type">{type}</div>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                  </div>
+                </a>
+              </Link>
+            </article>
+          ),
+        )}
     </div>
 
     <style jsx>{styles}</style>
-  </ExampleApp>
+  </App>
 );
 
 const homeQuery = gql`
   query Search($q: String) {
-    primoSearch(q: $q) {
+    primoSearch(search: $q) {
       id
       sourceId
       sourceRecordId
@@ -66,18 +73,20 @@ const homeQuery = gql`
 
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (ExamplePage)
-export default withData(graphql(homeQuery, {
-  options: ({ url: { query: { q } } }) => {
-    return {
-      variables: {
-        q,
-      },
-    };
-  },
-  props: ({ data, ownProps }) => {
-    return {
-      ...data,
-      items: ownProps.url.query.q && data.primoSearch,
-    };
-  },
-})(SearchPage));
+export default withData(
+  graphql(homeQuery, {
+    options: ({ url: { query: { q } } }) => {
+      return {
+        variables: {
+          q,
+        },
+      };
+    },
+    props: ({ data, ownProps }) => {
+      return {
+        ...data,
+        items: ownProps.url.query.q && data.primoSearch,
+      };
+    },
+  })(SearchPage),
+);
