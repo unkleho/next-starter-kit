b9e8599eb8691a5ee3788b1f32c95c075bd0f92c
Kaho Cheung <kaho@elefanttraks.com>
2018-03-13 10:39:45 +1100

    Updated collection item page

--- pages/collection/item.js
+++ pages/collection/item.js
@@ -2,28 +2,30 @@ import { gql, graphql } from 'react-apollo';
 
 import withData from '../../lib/withData';
 import App from '../../components/App';
-import Link from '../../components/Link';
+// import Link from '../../components/Link';
 import styles from './search.css';
 
-const CollectionItemPage = ({ url, item, loading: isLoading }) => (
+const CollectionItemPage = ({ item, loading: isLoading }) => (
   <App pathname="/search" isLoading={isLoading} title="Collection Item">
     {item && (
       <div className="collection-item container container--md">
+        {item.images.map((image) => {
+          return (
+            <img
+              src={image.url}
+              className="collection-item__image"
+              style={{
+                width: '100%',
+              }}
+            />
+          );
+        })}
+
         <h1>{item.title}</h1>
+
+        <p>{item.description}</p>
       </div>
     )}
-
-    {item.images.map((image) => {
-      return (
-        <img
-          src={image.url}
-          className="collection-item__image"
-          style={{
-            width: '100%',
-          }}
-        />
-      );
-    })}
 
     <style jsx>{styles}</style>
   </App>
