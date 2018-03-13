308155274c10cb311f1b5500a1da4b2699d07f14
Kaho Cheung <kaho@elefanttraks.com>
2018-03-14 10:48:30 +1100

    Updated collection item. Added react-photoswipe.

--- pages/collection/item.js
+++ pages/collection/item.js
@@ -1,35 +1,84 @@
 import { gql, graphql } from 'react-apollo';
+import { PhotoSwipeGallery } from 'react-photoswipe';
+import Head from 'next/head';
 
 import withData from '../../lib/withData';
 import App from '../../components/App';
 // import Link from '../../components/Link';
 import styles from './search.css';
 
-const CollectionItemPage = ({ item, loading: isLoading }) => (
-  <App pathname="/search" isLoading={isLoading} title="Collection Item">
-    {item && (
-      <div className="collection-item container container--md">
-        {item.images.map((image) => {
-          return (
-            <img
-              src={image.url}
-              className="collection-item__image"
-              style={{
-                width: '100%',
+const CollectionItemPage = ({ item, loading: isLoading }) => {
+  const images =
+    item &&
+    item.images.map((image) => {
+      return {
+        src: image.url,
+        thumbnail: image.url,
+        w: 2000,
+        h: 3000,
+      };
+    });
+
+  return (
+    <App pathname="/search" isLoading={isLoading} title="Collection Item">
+      <Head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="/static/styles/photoswipe.css"
+        />
+      </Head>
+
+      {item && (
+        <div className="collection-item container container--md">
+          {item.images && (
+            <PhotoSwipeGallery
+              items={images}
+              thumbnailContent={(image) => {
+                return (
+                  <img
+                    src={image.src}
+                    className="collection-item__image"
+                    // style={style}
+                    alt="This should be something meaningful"
+                  />
+                );
               }}
             />
-          );
-        })}
+          )}
 
-        <h1>{item.title}</h1>
+          <p>{item.type}</p>
+          <h1>{item.title}</h1>
 
-        <p>{item.description}</p>
-      </div>
-    )}
+          <p>{item.description}</p>
+        </div>
+      )}
 
-    <style jsx>{styles}</style>
-  </App>
-);
+      <style jsx>{styles}</style>
+      <style jsx global>
+        {`
+          .pswp-thumbnails {
+            display: flex;
+            flex-wrap: wrap;
+          }
+
+          .pswp-thumbnail {
+            width: 10%;
+            margin-right: 3px;
+          }
+
+          .pswp-thumbnail:first-child {
+            width: 100%;
+          }
+
+          .pswp-thumbnail img {
+            width: 100%;
+          }
+        `}
+      </style>
+    </App>
+  );
+};
 
 const query = gql`
   query Search($id: String) {
@@ -59,7 +108,7 @@ export default withData(
       };
     },
     props: ({ data }) => {
-      console.log(data.primoRecord);
+      // console.log(data.primoRecord);
       return {
         ...data,
         item: data.primoRecord,
