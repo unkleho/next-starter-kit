d11bb81f2a4aabdfa5cfeea50abc2a4828f1c3b5
Kaho Cheung <kaho@elefanttraks.com>
2018-12-21 23:54:00 +1100

    Fix jest errors

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	setupFiles: ['<rootDir>/jest.setup.js'],
+	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+	transform: {
+		// Prevent 'Unexpected token import' errors
+		'^.+\\.js$': 'babel-jest',
+		// Ensure Jest can use next-css style imports eg. import './style.css'.
+		'.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css',
+		// Prevent css module parse errors
+		'.*': '<rootDir>/node_modules/jest-css-modules',
+	},
+};
