939004fa8aceb624de60a6d4e99960d242db7d0d
Kaho Cheung <kaho@elefanttraks.com>
2019-01-04 21:30:26 +1100

    Add HeadMetaFields

--- components/examples/ExampleApp.js
+++ components/examples/ExampleApp.js
@@ -1,13 +1,35 @@
 import Head from 'next/head';
 
+import HeadMetaFields from '../HeadMetaFields';
+
 import '../../styles/base.css';
 import '../../styles/helpers.css';
 
-export default ({ children }) => {
+export default ({
+	children,
+	title,
+	description,
+	url,
+	imageUrl,
+	imageAlt,
+	siteName,
+	fbAppId,
+	twitterUsername,
+}) => {
 	return (
 		<main>
 			<Head>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<HeadMetaFields
+					title={title}
+					description={description}
+					url={url}
+					imageUrl={imageUrl}
+					imageAlt={imageAlt}
+					siteName={siteName}
+					fbAppId={fbAppId}
+					twitterUsername={twitterUsername}
+				/>
 			</Head>
 			{children}
 		</main>
