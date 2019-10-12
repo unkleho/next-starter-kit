f119609ddc8415fd6a93f794db50ac874d93ce2e
Kaho Cheung <kaho@elefanttraks.com>
2019-10-13 01:08:07 +1100

    Add portfolio data

---
+++ data/portfolio.ts
@@ -0,0 +1,83 @@
+const portfolio = {
+	works: [
+		{
+			title: 'MAAS Collection',
+			type: 'website',
+			description: null,
+			year: 2017,
+			tags: ['web-development', 'web-design'],
+		},
+		{
+			title: 'Data Explorer',
+			type: 'website',
+			description: null,
+			year: 2018,
+			tags: ['web-development', 'web-design'],
+		},
+		{
+			title: 'DX Lab',
+			type: 'website',
+			description: null,
+			year: 2018,
+			tags: ['web-development', 'web-design'],
+		},
+		{
+			title: 'Meridian',
+			type: 'website',
+			description: null,
+			year: 2017,
+			tags: ['web-development', 'web-design'],
+		},
+		{
+			title: '#NewSelfWales',
+			type: 'website',
+			description: null,
+			year: 2018,
+			tags: ['web-development', 'web-design'],
+		},
+		{
+			title: 'Unkle Ho: Roads to Roma',
+			type: 'album',
+			description: null,
+			year: 2003,
+			tags: ['music', 'graphic-design'],
+		},
+		{
+			title: 'Unkle Ho: Circus Maximus',
+			type: 'album',
+			description: null,
+			year: 2005,
+			tags: ['music', 'graphic-design'],
+		},
+		{
+			title: 'Unkle Ho: Subterranea',
+			type: 'album',
+			description: null,
+			year: 2009,
+			tags: ['music', 'graphic-design'],
+		},
+		{
+			title: 'Hermitude: Tales of the Drift',
+			type: 'album',
+			description: null,
+			year: null,
+			tags: ['graphic-design'],
+		},
+		{
+			title: 'Hermitude: Threads',
+			type: 'album',
+			description: null,
+			year: null,
+			tags: ['graphic-design'],
+		},
+		{
+			title: 'ILMU Festival',
+			type: 'event',
+			description: null,
+			year: null,
+			tags: ['graphic-design', 'event'],
+		},
+	],
+};
+
+export default portfolio;
